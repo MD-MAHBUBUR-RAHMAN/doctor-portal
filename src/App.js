4d6fcc8ba1488195f1f1f62976b28b54981b136f
MD-MAHBUBUR-RAHMAN <nirjhor211288@gmapl.com>
2022-05-12 01:07:27 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Page/Home/Home/Home";
+import About from "./Page/About/About";
+import Nav from "./Page/Shared/Nav";
+import Login from "./Page/Login/Login";
+import NotFound from "./Page/Notfound/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Page/Notfound/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="text-4xl text-center text-red-600">
+      <h1>404</h1>
+    </div>
+  );
+};
+
+export default NotFound;
